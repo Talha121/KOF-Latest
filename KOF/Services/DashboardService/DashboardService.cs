dec2161185b9282c7a93310976504a3eb7b7e93d
toqeerzia123 <41077688+toqeerzia123@users.noreply.github.com>
2022-02-22 00:56:33 +0500

    Profit on completed orders

--- KOF/Services/DashboardService/DashboardService.cs
+++ KOF/Services/DashboardService/DashboardService.cs
@@ -25,12 +25,12 @@ namespace KOF.Services.DashboardService
             data.TotalOrders= orders.Count();
 
             var totalordersitems = await _context.OrderItems.ToListAsync();
-            data.TotalSale = totalordersitems.Sum(x => x.TotalPrice);
-            data.TodaySale = totalordersitems.Where(x => Convert.ToDateTime(x.CreatedOn).Date == DateTime.Now.Date).Sum(x => x.TotalPrice);
-            var purchaseprice = totalordersitems.Sum(x => x.TotalCost);
-            var todatpurchaseprice = totalordersitems.Where(x => Convert.ToDateTime(x.CreatedOn).Date == DateTime.Now.Date).Sum(x => x.TotalCost);
-            data.TodayProfit =data.TodaySale-purchaseprice;
-            data.totalProfit = data.TotalSale - todatpurchaseprice;
+            data.TotalSale = totalordersitems.Where(x=>x.Order.OrderStatus== "Completed").Sum(x => x.TotalPrice);
+            data.TodaySale = totalordersitems.Where(x => Convert.ToDateTime(x.CreatedOn).Date == DateTime.Now.Date&&x.Order.OrderStatus == "Completed").Sum(x => x.TotalPrice);
+            var totalpurchaseprice = totalordersitems.Where(x => x.Order.OrderStatus == "Completed").Sum(x => x.TotalCost);
+            var todaypurchaseprice = totalordersitems.Where(x => Convert.ToDateTime(x.CreatedOn).Date == DateTime.Now.Date&&x.Order.OrderStatus == "Completed").Sum(x => x.TotalCost);
+            data.TodayProfit =data.TodaySale- todaypurchaseprice;
+            data.totalProfit = data.TotalSale - totalpurchaseprice;
          
             return data;
         }
@@ -38,8 +38,8 @@ namespace KOF.Services.DashboardService
         public async Task<MonthlySaleDto> MonthlySale(Datedto dto)
         {
             MonthlySaleDto data = new MonthlySaleDto();
-            var items =  _context.OrderItems.ToList();
-            var orders = _context.Orders.ToList();
+            var items =  _context.OrderItems.Where(x => x.Order.OrderStatus == "Completed").ToList();
+            var orders = _context.Orders.Where(x => x.OrderStatus == "Completed").ToList();
             data.Orders = orders.Where(x=>Convert.ToDateTime(x.CreatedOn).Date>=dto.FromDate.Date&& Convert.ToDateTime(x.CreatedOn).Date<=dto.ToDate).Count();
             var dateitems = items.Where(x => Convert.ToDateTime(x.CreatedOn).Date >= dto.FromDate.Date && Convert.ToDateTime(x.CreatedOn).Date <= dto.ToDate);
             data.Sale = dateitems.Sum(x => x.TotalPrice);
